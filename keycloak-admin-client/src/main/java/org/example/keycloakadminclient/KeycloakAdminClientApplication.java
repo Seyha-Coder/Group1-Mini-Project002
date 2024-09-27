df124531e39ea7bfb2e8bc8c777b4b2468e8177b
Chan Seyha <user.email>
2024-09-27 09:27:46 +0700

    change realm json

--- keycloak-admin-client/src/main/java/org/example/keycloakadminclient/KeycloakAdminClientApplication.java
+++ keycloak-admin-client/src/main/java/org/example/keycloakadminclient/KeycloakAdminClientApplication.java
@@ -30,7 +30,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
         flows = @OAuthFlows(
                 clientCredentials = @OAuthFlow(
                         tokenUrl ="http://localhost:8080/realms/Mini-Project002/protocol/openid-connect/token"
-                ),
+                )
+                ,
                 password = @OAuthFlow(
                         tokenUrl ="http://localhost:8080/realms/Mini-Project002/protocol/openid-connect/token"
                 )
